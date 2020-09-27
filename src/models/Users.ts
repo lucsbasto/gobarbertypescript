2ff4f97c8c615c544ea3a716296fd278a6e23053
Lucas Bastos <lucas.b@adireto.com>
2020-09-27 17:00:12 -0300

    change id field to uuid

--- src/models/Users.ts
+++ src/models/Users.ts
@@ -1,7 +1,7 @@
 import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from 'typeorm';
 
 @Entity('users')
-class Users{
+export default class Users{
   @PrimaryGeneratedColumn('uuid')
   id: string;
 
